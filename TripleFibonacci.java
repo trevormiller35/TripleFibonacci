90332df73b506d80a4d911290cb7f11e1d0b5894
trevormiller35 <51308372+trevormiller35@users.noreply.github.com>
2019-07-01 08:04:31 -0500

    Update TripleFibonacci.java
    Checked that input was appropriate. Also added a loop so the person could check multiple answers and quit out when they were done.

--- TripleFibonacci.java
+++ TripleFibonacci.java
@@ -23,20 +23,45 @@ public class TripleFibonacci {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        Scanner s = new Scanner(System.in);
-        ArrayList<Integer> sequence = new ArrayList();
-        sequence.add(1);
-        sequence.add(1);
-        sequence.add(1);
-        System.out.println("How many places out would you like to see?");
-        int maximum = s.nextInt();
-        System.out.println("1: 1");
-        System.out.println("2: 1");
-        System.out.println("3: 1");
-        for (int y = 3; y < maximum; y++) {
-            sequence.add(sequence.get(y - 3) + sequence.get(y - 2) + sequence.get(y - 1));
-            System.out.println((y + 1) + ": " + sequence.get(y));
+        boolean condition = true;
+        while (condition) {
+            
+        
+            Scanner s = new Scanner(System.in);
+            ArrayList<Integer> sequence = new ArrayList();
+            sequence.add(1);
+            sequence.add(1);
+            sequence.add(1);
+            System.out.println("How many places out would you like to see?");
+            int maximum = 0;
+            if (!s.hasNextInt()) {
+                System.out.println("Please use whole numbers.");
+                s.reset();
+            } else {
+                maximum = s.nextInt();
+                if (maximum <= 0) {
+                    System.out.println("Please answer with a positive integer.");
+                } else if (maximum == 1){
+                    System.out.println("1: 1");
+                } else if (maximum == 2) {
+                    System.out.println("1: 1");
+                    System.out.println("2: 1");
+                } else if (maximum > 2) {
+                    System.out.println("1: 1");
+                    System.out.println("2: 1");
+                    System.out.println("3: 1");
+                    for (int y = 3; y < maximum; y++) {
+                        sequence.add(sequence.get(y - 3) + sequence.get(y - 2) + sequence.get(y - 1));
+                        System.out.println((y + 1) + ": " + sequence.get(y));
+                    }
+                }
+                System.out.println("Do you want to go again? Y/N");
+                if (s.next().equalsIgnoreCase("n")) {
+                    condition = false;
+                }
+            }
         }
+        
     }
     
 }
